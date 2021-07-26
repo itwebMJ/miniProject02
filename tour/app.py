527e8c5e33fcc8096a8d281850122d4a48db8155
itwebMJ <rlaalwn61@naver.com>
2021-07-26 14:06:25 +0900

    add alert

--- tour/app.py
+++ tour/app.py
@@ -1,13 +1,12 @@
-from flask import Flask, request, render_template
+from flask import Flask, render_template
 import routes.location_route as rl
 import routes.coronainfo_route as corona
 import routes.member_route as mr
 import routes.board_route as br
-import models.board as b
+import tour.models.board as b
 
 
 app = Flask(__name__, template_folder="templates")
-app.secret_key = "cggasadfsgadf" #시크릿키 설정
 
 #생성한 블루프린트 등록
 app.register_blueprint(rl.bp)
@@ -22,6 +21,7 @@ def home() :
     return render_template("/index.html", bd = bd)
 
 if __name__ == '__main__':
-    app.debug=True
+    app.debug = True
+    app.secret_key = 'super secret key'
     app.config['SESSION_TYPE'] = 'filesystem'
     app.run()#flask 서버 실행
